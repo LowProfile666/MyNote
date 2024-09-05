63f99de308591917303f806096780d396b99b85d
ZSM <2062008467@qq.com>
2024-09-05 23:17:35 +0800

    2024.09.05 23:17:33

--- RunAutoPushByWindow.bat
+++ RunAutoPushByWindow.bat
@@ -5,4 +5,4 @@ REM 将反斜杠替换为正斜杠
 set SCRIPT_DIR=%SCRIPT_DIR:\=/%
 
 REM 调用 Git Bash 并执行 AutoPush.sh 脚本
-"D:\Git\bin\bash.exe" -c "sh %SCRIPT_DIR%AutoPush.sh"
+"D:\Git\bin\bash.exe" -c "sh %SCRIPT_DIR%AutoPushByWindow.sh"
